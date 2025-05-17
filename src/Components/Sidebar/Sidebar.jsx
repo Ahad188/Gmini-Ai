fb7346304a808204be50354d85e7a39261a2e144
AhaD <abuahad35188@gmail.com>
2025-05-18 00:20:17 +0600

    colpse in sidebar and also addd font size

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -5,37 +5,45 @@ import help from '../../assets/question_icon.png';
 import history from '../../assets/history_icon.png';
 import settings from '../../assets/setting_icon.png';
 import './sidebar.css'
+import { useState } from 'react';
 
 const Sidebar = () => {
+     const [extended,setExtended] = useState(false)
      return (
           <div className='sidebar'>
                <div className="top">
-                    <img className='menu' src={menu} alt="" />
+                    <img onClick={()=>setExtended(!extended)} className='menu' src={menu} alt="" />
                     <div className="new-chat">
                          <img className='plus' src={plus} alt="" />
-                         <p>New Chat</p>
+                         {extended ? <p>New Chat</p> : '' }
+                          
                     </div>
-                    <div className="recent">
+                    {
+                         extended ?  <div className="recent">
                          <p className="recent-title">Recent</p>
                          <div className="recent-entry">
                               <img src={message} alt="" />
                               <p>what is react .....</p>
                          </div>
-                    </div>
+                    </div> : " "
+                    }
                </div>
 
                <div className="bottom">
                     <div className="bottom-item recent-entry">
                          <img src={help} alt="" />
-                         <p>Help</p>
+                         {
+                              extended ? <p>Help</p> : ' '
+                         }
                     </div>
                     <div className="bottom-item recent-entry">
                          <img src={history} alt="" />
+                         {extended ? <p>History</p> : " "}
                          <p>History</p>
                     </div>
                     <div className="bottom-item recent-entry">
                          <img src={settings} alt="" />
-                         <p>Settings</p>
+                         {extended ? <p>Setting</p> : ""}
                     </div>
                </div>
           </div>
